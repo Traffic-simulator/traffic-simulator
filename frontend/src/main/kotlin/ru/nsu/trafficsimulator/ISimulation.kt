e7d283e8e8bdc1b177209721467dc75038a424cd
Egor Kuklin <40146818+TheBlek@users.noreply.github.com>
2024-11-17 20:21:34 +0700

    First draft for interface with backend (#4)
    * First draft for interface with backend
    
    * Updated interface
    
    * Update naming
    
    * Update Vehiclype enum

---
+++ frontend/src/main/kotlin/ru/nsu/trafficsimulator/ISimulation.kt
@@ -0,0 +1,41 @@
+package ru.nsu.trafficsimulator
+
+import opendrive.OpenDRIVE
+import opendrive.TRoad
+
+interface ISimulation {
+    /**
+     * All possible cars types that behave differently on the road
+     * Each car type might have multiple variations as per supplied information at init time
+     * 0th variation is default and guaranteed to exist
+     */
+    enum class VehicleType {
+        PassengerCar,
+        Bus;
+
+        var variation: Int = 0
+    }
+
+    /**
+     * Class for communicating information about vehicles and their positions of the road
+     * Distance specifies distance from the start of the road
+     * Distance should be positive, and less than length of the road
+     */
+    data class VehicleDTO(val id: Int, val road: TRoad, val laneId: Int, val type: VehicleType, val distance: Double)
+
+    /**
+     * Initialize simulation state with.
+     * All information about points of interest, global settings, road rule settings
+     * are embedded inside gAdditionalData list with type TUserData as specific elements
+     * This method should not throw but return error as a return value
+     * TODO: specify userData layout
+     * @param layout Layout to initialize simulation
+     */
+    fun init(layout: OpenDRIVE): Error?
+
+    /**
+     * Calculate next vehicle positions after a set amount of time
+     * @param deltaTime time interval to simulate
+     */
+    fun getNextFrame(deltaTime: Double): Array<VehicleDTO>
+}
