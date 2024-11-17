8b327bd15d3b5d82082c0c214b8ea4d8bdf6cf98
Egor <egorka99966@gmail.com>
2024-11-17 19:56:01 +0700

    Updated interface

--- frontend/src/main/kotlin/ru/nsu/trafficsimulator/Backend.kt
+++ frontend/src/main/kotlin/ru/nsu/trafficsimulator/Backend.kt
@@ -8,11 +8,21 @@ import opendrive.TRoadLanesLaneSectionLrLane
 
 interface Backend {
     /**
+     * All possible cars variations that behave differently on the road
+     */
+    enum class VehicleType {
+        PassengerCar,
+        Truck,
+        Bus,
+        Motorcycle,
+    }
+
+    /**
      * Class for communicating information about vehicles and their positions of the road
      * Distance specifies distance from the start of the road
      * Distance should be positive, and less than length of the road
      */
-    data class Vehicle(val id: Int, val road: Road, val laneId: Int, val distance: Double)
+    data class Vehicle(val id: Int, val road: TRoad, val laneId: Int, val type: VehicleType, val distance: Double)
 
     /**
      * Initialize simulation state with.
@@ -25,7 +35,8 @@ interface Backend {
     fun init(layout: OpenDRIVE): Error?
 
     /**
-     * Calculate next vehicle positions
+     * Calculate next vehicle positions after a set amount of time
+     * @param deltaTime time interval to simulate
      */
-    fun getNextFrame(): Array<Vehicle>
+    fun getNextFrame(deltaTime: Double): Array<Vehicle>
 }
