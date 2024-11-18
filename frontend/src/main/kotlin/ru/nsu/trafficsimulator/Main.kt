d60fb52d6fdb32c2dbf4c801dda435993b9b710f
PalkinEgor <palkin122004@gmail.com>
2024-11-18 23:08:34 +0700

    add driving cars

--- frontend/src/main/kotlin/ru/nsu/trafficsimulator/Main.kt
+++ frontend/src/main/kotlin/ru/nsu/trafficsimulator/Main.kt
@@ -7,30 +7,49 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics
 import com.badlogic.gdx.graphics.*
 import com.badlogic.gdx.graphics.g3d.Environment
 import com.badlogic.gdx.graphics.g3d.Model
+import com.badlogic.gdx.graphics.g3d.ModelBatch
+import com.badlogic.gdx.graphics.g3d.ModelInstance
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
+import com.badlogic.gdx.math.Vector3
 import imgui.ImGui
 import imgui.gl3.ImGuiImplGl3
 import imgui.glfw.ImGuiImplGlfw
 import net.mgsx.gltf.loaders.glb.GLBLoader
-import net.mgsx.gltf.scene3d.scene.Scene
 import net.mgsx.gltf.scene3d.scene.SceneAsset
 import net.mgsx.gltf.scene3d.scene.SceneManager
+import kotlin.math.cos
+import kotlin.math.sin
+
 
 class Main : ApplicationAdapter() {
     private var image: Texture? = null
     private var imGuiGlfw: ImGuiImplGlfw = ImGuiImplGlfw()
     private var imGuiGl3: ImGuiImplGl3 = ImGuiImplGl3()
-    private var color = floatArrayOf(0.0f, 0.0f, 0.0f)
     private var camera: PerspectiveCamera? = null
-    private var model: Model? = null
     private var environment: Environment? = null
     private var sceneManager: SceneManager? = null
-    private var sceneAsset: SceneAsset? = null
+    private var sceneAsset1: SceneAsset? = null
+    private var sceneAsset2: SceneAsset? = null
     private var tmpInputProcessor: InputProcessor? = null
 
+    private var carX = floatArrayOf(0.0f)
+    private var carY = floatArrayOf(0.0f)
+    private var carZ = floatArrayOf(0.0f)
+    private var elapsedTime = 0f
+    private var rotationAngle = 1f
+
+    private var model1: Model? = null
+    private var modelInstance1: ModelInstance? = null
+    private var modelBatch1: ModelBatch? = null
+
+    private var model2: Model? = null
+    private var modelInstance2: ModelInstance? = null
+    private var modelBatch2: ModelBatch? = null
+
     override fun create() {
+        image = Texture("libgdx.png")
         val windowHandle = (Gdx.graphics as Lwjgl3Graphics).window.windowHandle
         ImGui.createContext()
         ImGui.styleColorsDark()
@@ -49,10 +68,15 @@ class Main : ApplicationAdapter() {
         environment!!.add(DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, 0.8f, -0.2f))
 
         sceneManager = SceneManager()
-        sceneAsset = GLBLoader().load(Gdx.files.internal("car.glb"))
-        sceneManager?.addScene(Scene(sceneAsset?.scene))
+        sceneAsset1 = GLBLoader().load(Gdx.files.internal("cop.glb"))
+        sceneAsset2 = GLBLoader().load(Gdx.files.internal("racer.glb"))
         sceneManager?.setCamera(camera)
         sceneManager?.environment = environment
+
+        modelInstance1 = ModelInstance(sceneAsset1?.scene?.model)
+        modelBatch1 = ModelBatch()
+        modelInstance2 = ModelInstance(sceneAsset2?.scene?.model)
+        modelBatch2 = ModelBatch()
 
         val camController = CameraInputController(camera);
         Gdx.input.inputProcessor = camController;
@@ -67,9 +91,13 @@ class Main : ApplicationAdapter() {
         imGuiGlfw.newFrame()
         ImGui.newFrame()
         run {
-            ImGui.begin("Hello world!")
-            ImGui.text("Choose color for background:")
-            ImGui.colorEdit3("background", color)
+            ImGui.begin("Position")
+            ImGui.text("Choose x value")
+            ImGui.sliderFloat("x", carX, -20.0f, 20.0f)
+            ImGui.text("Choose y value")
+            ImGui.sliderFloat("y", carY, -20.0f, 20.0f)
+            ImGui.text("Choose z value")
+            ImGui.sliderFloat("z", carZ, -20.0f, 20.0f)
             ImGui.end()
         }
         ImGui.render()
@@ -78,19 +106,34 @@ class Main : ApplicationAdapter() {
             Gdx.input.inputProcessor = null
         }
 
+        elapsedTime += Gdx.graphics.deltaTime
+        modelInstance1?.transform?.setTranslation(5 * sin(elapsedTime), carY[0], 5 * cos(elapsedTime))
+        modelInstance1?.transform?.rotate(Vector3(0f, 1f, 0f), rotationAngle)
+        modelInstance2?.transform?.setTranslation(5 * sin(elapsedTime + 1), carY[0], 5 * cos(elapsedTime + 1))
+        modelInstance2?.transform?.rotate(Vector3(0f, 1f, 0f), rotationAngle)
+
         Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height);
-        Gdx.gl.glClearColor(color[0], color[1], color[2], 0.0f)
+        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
 
         sceneManager?.update(Gdx.graphics.deltaTime)
         sceneManager?.render()
+
+        modelBatch1?.begin(camera)
+        modelBatch1?.render(modelInstance1, environment)
+        modelBatch1?.end()
+
+        modelBatch2?.begin(camera)
+        modelBatch2?.render(modelInstance2, environment)
+        modelBatch2?.end()
 
         imGuiGl3.renderDrawData(ImGui.getDrawData())
     }
 
     override fun dispose() {
         image?.dispose()
-        model?.dispose()
+        model1?.dispose()
+        model2?.dispose()
         imGuiGl3.shutdown()
         imGuiGlfw.shutdown()
         ImGui.destroyContext()
