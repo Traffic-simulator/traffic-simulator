14f3179eb646516bfc533361682452852313beb3
Anatoly Savin <a.savin1@g.nsu.ru>
2024-12-05 01:51:38 +0700

    draw 1 car, single_segment_road.xodr

--- frontend/src/main/kotlin/ru/nsu/trafficsimulator/Main.kt
+++ frontend/src/main/kotlin/ru/nsu/trafficsimulator/Main.kt
@@ -1,5 +1,8 @@
 package ru.nsu.trafficsimulator;
 
+import BackendAPI
+import OpenDriveReader
+import SpawnDetails
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputProcessor
@@ -19,8 +22,9 @@ import imgui.glfw.ImGuiImplGlfw
 import net.mgsx.gltf.loaders.glb.GLBLoader
 import net.mgsx.gltf.scene3d.scene.SceneAsset
 import net.mgsx.gltf.scene3d.scene.SceneManager
-import kotlin.math.cos
-import kotlin.math.sin
+import opendrive.TRoad
+import vehicle.Direction
+import kotlin.math.*
 
 
 class Main : ApplicationAdapter() {
@@ -48,6 +52,12 @@ class Main : ApplicationAdapter() {
     private var modelInstance2: ModelInstance? = null
     private var modelBatch2: ModelBatch? = null
 
+    var hasRotated = false
+    var currentRoad: TRoad? = null
+    var currentGeometryIndex = 0
+
+    private val back = BackendAPI()
+
     override fun create() {
         image = Texture("libgdx.png")
         val windowHandle = (Gdx.graphics as Lwjgl3Graphics).window.windowHandle
@@ -56,9 +66,11 @@ class Main : ApplicationAdapter() {
         imGuiGlfw.init(windowHandle, true)
         imGuiGl3.init()
 
+        // Устанавливаем камеру сверху
         camera = PerspectiveCamera(67f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
-        camera?.position?.set(10f, 10f, 10f)
-        camera?.lookAt(0.0f, 0.0f, 0.0f)
+        camera?.position?.set(-40f, 20f, 240f)  // Камера будет находиться 20 единиц выше по оси Y
+        camera?.lookAt(-40f, 0.0f, 240.0f)  // Камера будет смотреть вниз, в центр сцены
+        camera?.up?.set(0f, 0f, -1f)  // Устанавливаем, что "вверх" будет по оси Z, чтобы камера смотрела вниз
         camera?.near = 1f
         camera?.far = 300f
         camera?.update()
@@ -77,6 +89,37 @@ class Main : ApplicationAdapter() {
         modelBatch1 = ModelBatch()
         modelInstance2 = ModelInstance(sceneAsset2?.scene?.model)
         modelBatch2 = ModelBatch()
+
+        val odr = OpenDriveReader()
+        val openDRIVE = odr.read("single_segment_road.xodr")
+//        println(openDRIVE.road.size)
+
+//        val openDRIVE = odr.read("single_road.xodr")
+
+        println(openDRIVE.road.size)
+
+//        val spawnDetails = ArrayList<Triple<String, String, Direction>>()
+//        spawnDetails.add(Triple("0", "1", Direction.FORWARD))
+//        spawnDetails.add(Triple("0", "2", Direction.FORWARD))
+//        spawnDetails.add(Triple("0", "3", Direction.FORWARD))
+//        spawnDetails.add(Triple("0", "4", Direction.FORWARD))
+
+        val spawnDetails = ArrayList<Triple<String, String, Direction>>()
+        spawnDetails.add(Triple("57", "1", Direction.BACKWARD))
+        spawnDetails.add(Triple("57", "2", Direction.BACKWARD))
+        spawnDetails.add(Triple("21", "-1", Direction.FORWARD))
+        spawnDetails.add(Triple("21", "-2", Direction.FORWARD))
+
+        /*val spawnDetails = ArrayList<Triple<String, String, Direction>>()
+        spawnDetails.add(Triple("0", "1", Direction.BACKWARD))
+        spawnDetails.add(Triple("0", "2", Direction.BACKWARD))
+        spawnDetails.add(Triple("0", "3", Direction.BACKWARD))
+        spawnDetails.add(Triple("0", "4", Direction.BACKWARD))
+        spawnDetails.add(Triple("0", "-1", Direction.FORWARD))
+        spawnDetails.add(Triple("0", "-2", Direction.FORWARD))*/
+
+//        Еще один пример для карты без перекрестка
+        back.init(openDRIVE, SpawnDetails(spawnDetails), 228)
 
         val camController = CameraInputController(camera);
         Gdx.input.inputProcessor = camController;
@@ -107,39 +150,136 @@ class Main : ApplicationAdapter() {
         }
 
         elapsedTime += Gdx.graphics.deltaTime
-        modelInstance1?.transform?.setTranslation(5 * sin(elapsedTime), carY[0], 5 * cos(elapsedTime))
-        modelInstance1?.transform?.rotate(Vector3(0f, 1f, 0f), rotationAngle)
         modelInstance2?.transform?.setTranslation(5 * sin(elapsedTime + 1), carY[0], 5 * cos(elapsedTime + 1))
         modelInstance2?.transform?.rotate(Vector3(0f, 1f, 0f), rotationAngle)
 
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height);
-        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+        // Получение данных о положении автомобиля
+        val vehiclePositions = back.getNextFrame(0.005) // Интервал времени
+        val carPosition = vehiclePositions[0].distance
+        val newRoad = vehiclePositions[0].road
 
-        sceneManager?.update(Gdx.graphics.deltaTime)
-        sceneManager?.render()
+        // Если дорога изменилась, обновляем CarController
+        if (currentRoad != newRoad) {
+            currentRoad = newRoad
+        }
 
-        modelBatch1?.begin(camera)
-        modelBatch1?.render(modelInstance1, environment)
-        modelBatch1?.end()
+        // Обновляем позицию автомобиля
+        if (currentRoad != null) {
+            val roadLength = currentRoad!!.length
+            if (carPosition <= roadLength) {
+                val currentGeometry = currentRoad!!.planView.geometry[currentGeometryIndex]
 
-        modelBatch2?.begin(camera)
-        modelBatch2?.render(modelInstance2, environment)
-        modelBatch2?.end()
+                val geometryStart = currentGeometry.s
+                val geometryEnd = geometryStart + currentGeometry.length
+                if (carPosition in geometryStart..geometryEnd) {
+                    if (currentGeometry.line != null) {
+                        val roadX = currentGeometry.x
+                        val roadZ = currentGeometry.y
+                        val roadAngle = currentGeometry.hdg
 
-        imGuiGl3.renderDrawData(ImGui.getDrawData())
+                        val newPosX = roadX + (carPosition - currentGeometry.s) * cos(roadAngle)
+                        val newPosZ = roadZ + (carPosition - currentGeometry.s) * sin(roadAngle)
+
+                        modelInstance1?.transform?.setTranslation(newPosX.toFloat(), carY[0], newPosZ.toFloat())
+
+                        // Установка вращения автомобиля (если необходимо)
+                        if (!hasRotated) {
+                            val roadAngle = currentRoad!!.planView.geometry[currentGeometryIndex].hdg
+                            modelInstance1?.transform?.setToRotationRad(Vector3(0f, 1f, 0f), -roadAngle.toFloat())
+                            hasRotated = true
+                        }
+                    } else if (currentGeometry.arc != null) {
+                        val r = 1 / currentGeometry.arc.curvature
+                        val startAngle = currentGeometry.hdg
+                        val offset = carPosition - currentGeometry.s
+                        val endAngle = startAngle + currentGeometry.arc.curvature * offset
+
+                        val end = Point2(currentGeometry.x, currentGeometry.y) - Point2(
+                            sin(startAngle) - sin(endAngle),
+                            -cos(startAngle) + cos(endAngle)
+                        ) * r
+
+                        val newPosX = end.x
+                        val newPosZ = end.y
+
+                        modelInstance1?.transform?.setTranslation(newPosX.toFloat(), carY[0], newPosZ.toFloat())
+
+                        // Установка вращения автомобиля (если необходимо)
+                        if (!hasRotated) {
+                            val roadAngle = currentRoad!!.planView.geometry[currentGeometryIndex].hdg
+                            modelInstance1?.transform?.setToRotationRad(Vector3(0f, 1f, 0f), -roadAngle.toFloat())
+                            hasRotated = true
+                        }
+                    } else {
+                        null
+                    }
+                } else {
+                    // Если достигнут конец текущей геометрии, переключаемся на следующую
+                    if (currentGeometryIndex < currentRoad!!.planView.geometry.size - 1) {
+                        currentGeometryIndex++
+                        hasRotated = false // Сбрасываем флаг вращения
+                        println("\nконец геометрии ${currentGeometry.s}\n")
+                    } else {
+                        println("Машина достигла конца дороги ${currentRoad!!.id}")
+                    }
+                }
+            } else {
+                println("Машина достигла конца дороги ${currentRoad!!.id}")
+            }
+        }
+
+    Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height);
+    Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+
+    sceneManager?.update(Gdx.graphics.deltaTime)
+    sceneManager?.render()
+
+    modelBatch1?.begin(camera)
+    modelBatch1?.render(modelInstance1, environment)
+    modelBatch1?.end()
+
+    modelBatch2?.begin(camera)
+    modelBatch2?.render(modelInstance2, environment)
+    modelBatch2?.end()
+
+    imGuiGl3.renderDrawData(ImGui.getDrawData())
+}
+
+override fun dispose() {
+    image?.dispose()
+    model1?.dispose()
+    model2?.dispose()
+    imGuiGl3.shutdown()
+    imGuiGlfw.shutdown()
+    ImGui.destroyContext()
+}
+
+override fun resize(width: Int, height: Int) {
+    sceneManager?.updateViewport(width.toFloat(), height.toFloat());
+}
+}
+
+data class Point2(var x: Double, var y: Double) {
+    fun distance(other: Point2): Double {
+        val dx = x - other.x
+        val dy = y - other.y
+        return sqrt(dx * dx + dy * dy)
     }
 
-    override fun dispose() {
-        image?.dispose()
-        model1?.dispose()
-        model2?.dispose()
-        imGuiGl3.shutdown()
-        imGuiGlfw.shutdown()
-        ImGui.destroyContext()
+    fun rotate(angleRad: Double): Point2 {
+        return Point2(x * cos(angleRad) - y * sin(angleRad), x * sin(angleRad) + y * cos(angleRad))
     }
 
-    override fun resize(width: Int, height: Int) {
-        sceneManager?.updateViewport(width.toFloat(), height.toFloat());
+    operator fun plus(other: Point2): Point2 {
+        return Point2(x + other.x, y + other.y)
+    }
+
+    operator fun minus(other: Point2): Point2 {
+        return Point2(x - other.x, y - other.y)
+    }
+
+    operator fun times(scalar: Double): Point2 {
+        return Point2(x * scalar, y * scalar)
     }
 }
--- frontend/src/main/kotlin/ru/nsu/trafficsimulator/serializer/Serializer.kt
+++ frontend/src/main/kotlin/ru/nsu/trafficsimulator/serializer/Serializer.kt
@@ -27,54 +27,95 @@ class Serializer {
         return TRoad().apply {
             id = road.id.toString()
             length = road.length
-            junction = "-1"
+            junction = "-1" // Без соединений с перекрёстками
 
-            link = TRoadLink()
-            link.predecessor = TRoadLinkPredecessorSuccessor().apply {
-                elementType = ERoadLinkElementType.JUNCTION
-                elementId = road.startIntersection.id.toString()
+            // Тип дороги
+            type.add(TRoadType().apply {
+                s = 0.0
+                type = ERoadType.TOWN
+                country = "DE"
+            })
+
+            // План дороги (геометрия)
+            planView = TRoadPlanView().apply {
+                geometry.add(TRoadPlanViewGeometry().apply {
+                    s = 0.0
+                    x = 120.0
+                    y = 137.0
+                    hdg = Math.PI / 2 // 90 градусов в радианах
+                    length = road.length
+                    line = TRoadPlanViewGeometryLine() // Прямая линия
+                })
             }
-            link.successor = TRoadLinkPredecessorSuccessor().apply {
-                elementType = ERoadLinkElementType.JUNCTION
-                elementId = road.endIntersection.id.toString()
-            }
 
-            planView = generateRoadPlaneView(road)
-
-            lanes = TRoadLanes().apply {
-                laneOffset.add(TRoadLanesLaneOffset().apply {
+            // Профиль дороги
+            elevationProfile = TRoadElevationProfile().apply {
+                elevation.add(TRoadElevationProfileElevation().apply {
+                    s = 0.0
                     a = 0.0
                     b = 0.0
                     c = 0.0
                     d = 0.0
-                    s = 0.0
                 })
+            }
 
+            // Полосы
+            lanes = TRoadLanes().apply {
                 laneSection.add(TRoadLanesLaneSection().apply {
                     s = 0.0
 
-                    left = TRoadLanesLaneSectionLeft()
-                    for (i in 1..road.leftLane) {
-                        val leftLane = TRoadLanesLaneSectionLeftLane()
-                        leftLane.id = i.toBigInteger()
-                        left.lane.add(leftLane)
+                    left = TRoadLanesLaneSectionLeft().apply {
+                        for (i in 1..road.leftLane) {
+                            lane.add(TRoadLanesLaneSectionLeftLane().apply {
+                                id = i.toBigInteger()
+                                type = ELaneType.DRIVING
+                                level = TBool.FALSE
+                                borderOrWidth.add(TRoadLanesLaneSectionLrLaneWidth().apply {
+                                    sOffset = 0.0
+                                    a = 3.75
+                                    b = 0.0
+                                    c = 0.0
+                                    d = 0.0
+                                })
+                            })
+                        }
                     }
 
-                    center = TRoadLanesLaneSectionCenter()
-                    val centerLane = TRoadLanesLaneSectionCenterLane()
-                    centerLane.id = 0.toBigInteger()
-                    center.lane.add(centerLane)
+                    center = TRoadLanesLaneSectionCenter().apply {
+                        lane.add(TRoadLanesLaneSectionCenterLane().apply {
+                            id = 0.toBigInteger()
+                            type = ELaneType.DRIVING
+                            level = TBool.FALSE
+                            roadMark.add(TRoadLanesLaneSectionLcrLaneRoadMark().apply {
+                                sOffset = 0.0
+                                type = ERoadMarkType.BROKEN
+                                color = ERoadMarkColor.STANDARD
+                                width = 0.12
+                            })
+                        })
+                    }
 
-                    right = TRoadLanesLaneSectionRight()
-                    for (i in 1..road.rightLane) {
-                        val rightLane = TRoadLanesLaneSectionRightLane()
-                        rightLane.id = (-i).toBigInteger()
-                        right.lane.add(rightLane)
+                    right = TRoadLanesLaneSectionRight().apply {
+                        for (i in 1..road.rightLane) {
+                            lane.add(TRoadLanesLaneSectionRightLane().apply {
+                                id = (-i).toBigInteger()
+                                type = ELaneType.DRIVING
+                                level = TBool.FALSE
+                                borderOrWidth.add(TRoadLanesLaneSectionLrLaneWidth().apply {
+                                    sOffset = 0.0
+                                    a = 3.75
+                                    b = 0.0
+                                    c = 0.0
+                                    d = 0.0
+                                })
+                            })
+                        }
                     }
                 })
             }
         }
     }
+
 
     private fun serializeIntersectionRoad(road: IntersectionRoad): TRoad {
         return TRoad().apply {
@@ -151,7 +192,7 @@ fun main() {
 
     val od = ser.serialize(layout)
 
-    odr.write(od, "testOpenDrive")
+    odr.write(od, "testOpenDrive.xodr")
 
 
 }
