7089dd4cc1e4d94c25f5589aa7febada3272c9ec
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2025-05-07 01:57:04 +0700

    add building parser and tested it

---
+++ simulator/src/main/kotlin/BuildingsParser.kt
@@ -0,0 +1,38 @@
+import opendrive.OpenDRIVE
+import opendrive.TUserData
+import route_generator_new.BuildingTypes
+import route_generator_new.discrete_function.Building
+
+class BuildingsParser(val openDrive: OpenDRIVE) {
+
+    public fun getBuildings() : List<Building> {
+        val buildings: MutableList<Building> = mutableListOf()
+        for (junction in openDrive.junction) {
+            val buildingId = junction.id
+            lateinit var buildingType : BuildingTypes
+            var buildingCapacity : Int = -1
+            var buildingCurrentPeople : Int = -1
+            val additionalData = junction.gAdditionalData
+
+            for (data in additionalData) {
+                val tUser = data as TUserData
+                when (tUser.code) {
+                    "buildingType" -> buildingType = BuildingTypes.valueOf(tUser.value)
+                    "buildingCapacity" -> buildingCapacity = tUser.value.toInt()
+                    "buildingFullness" -> buildingCurrentPeople = tUser.value.toInt()
+                    else -> {
+                        throw Exception("unknown type + ${tUser.code}")
+                    }
+                }
+            }
+            if (buildingCapacity == -1 || buildingCurrentPeople == -1) {
+                throw Exception("buildingCapacity or buildingCurrentPeople has incorrect value")
+            }
+            buildings.add(Building(buildingType, buildingCapacity, buildingCurrentPeople, buildingId))
+        }
+
+        return buildings;
+    }
+
+
+}
--- xodr_parser/src/main/kotlin/OpenDriveReader.kt
+++ xodr_parser/src/main/kotlin/OpenDriveReader.kt
@@ -1,9 +1,11 @@
 import jakarta.xml.bind.JAXBContext
 import jakarta.xml.bind.JAXBException
 import opendrive.OpenDRIVE
+import java.io.File
 
 import java.io.FileReader
 import java.io.IOException
+import java.io.InputStream
 import java.nio.file.Paths
 
 class OpenDriveReader {
@@ -13,5 +15,15 @@ class OpenDriveReader {
 //        println(Paths.get("src/main/resources/$filename").toAbsolutePath())
         return context.createUnmarshaller()
             .unmarshal(FileReader("src/main/resources/$filename")) as OpenDRIVE
+    }
+
+    @Throws(JAXBException::class, IOException::class)
+    fun readUsingFileReader(inputStream: InputStream): OpenDRIVE {
+        val context: JAXBContext = JAXBContext.newInstance(OpenDRIVE::class.java)
+        val unmarshallData = context
+                                .createUnmarshaller()
+                                .unmarshal(inputStream)
+
+        return unmarshallData as OpenDRIVE
     }
 }
